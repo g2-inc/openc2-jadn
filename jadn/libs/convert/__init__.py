891bb7acb4d332b21d7241a0690071689d9a787f
Jerome Czachor <jerome.czachor@g2-inc.com>
2018-11-14 14:11:24 -0500

    Breakout html & markdown converters

--- jadn/libs/convert/__init__.py
+++ jadn/libs/convert/__init__.py
@@ -1,5 +1,6 @@
 from .w_base import base_dump, base_dumps
 from .w_cddl import cddl_dump, cddl_dumps
+from .w_html import html_dump, html_dumps
 from .w_jas import jas_dump, jas_dumps
 from .w_proto import proto_dump, proto_dumps
 from .w_relax import relax_dump, relax_dumps
@@ -10,6 +11,8 @@ __all__ = [
     'base_dumps',
     'cddl_dump',
     'cddl_dumps',
+    'html_dump',
+    'html_dumps',
     'jas_dump',
     'jas_dumps',
     'proto_dump',
---
+++ jadn/libs/convert/w_html.py
@@ -0,0 +1,393 @@
+import json
+import re
+import os
+
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+from ..codec.codec_utils import fopts_s2d, topts_s2d
+from ..utils import Utils
+
+
+class JADNtoHTML(object):
+    def __init__(self, jadn):
+        """
+        Schema Converter for JADN to CDDL
+        :param jadn: str or dict of the JADN schema
+        :type jadn: str or dict
+        """
+        if type(jadn) is str:
+            try:
+                jadn = json.loads(jadn)
+            except Exception as e:
+                raise e
+        elif type(jadn) is dict:
+            pass
+
+        else:
+            raise TypeError('JADN improperly formatted')
+
+        self.indent = '  '
+
+        self._fieldMap = {
+            'Binary': 'bstr',
+            'Boolean': 'bool',
+            'Integer': 'int64',
+            'Number': 'float64',
+            'Null': 'null',
+            'String': 'bstr'
+        }
+
+        self._structFormats = {
+            'Record': self._formatRecord,
+            'Choice': self._formatChoice,
+            'Map': self._formatMap,
+            'Enumerated': self._formatEnumerated,
+            'Array': self._formatArray,
+            'ArrayOf': self._formatArrayOf,
+        }
+
+        self._meta = jadn['meta'] or {}
+        self._types = []
+        self._custom = []
+        self._customFields = []
+
+        for t in jadn['types']:
+            self._customFields.append(t[0])
+            if t[1] in self._structFormats.keys():
+                self._types.append(t)
+
+            else:
+                self._custom.append(t)
+
+    def indent_html(self, elem, level=0):
+        i = '\n' + level * '  '
+        if len(elem):
+            if not hasattr(elem, 'text') or not elem.text.strip():
+                elem.text = i + '  '
+
+            if not hasattr(elem, 'tail') or not elem.tail.strip():
+                elem.tail = i
+
+            for elem in elem:
+                self.indent_html(elem, level + 1)
+
+            if not hasattr(elem, 'tail') or not elem.tail.strip():
+                elem.tail = i
+        else:
+            if level and (not elem.tail or not elem.tail.strip()):
+                elem.tail = i
+
+    def html_dump(self):
+        html = BeautifulSoup('''
+            <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <link rel="stylesheet" type="text/css" href="theme.css">
+                    <title>{title} v.{version}</title>
+                </head>
+                <body>
+                </body>
+            </html>
+        '''.format(
+            title=self._meta.get('module', 'JADN Schema Conversion'),
+            version=self._meta.get('version', '0'),
+        ), 'lxml')
+
+        html.body.append(self.makeHeader())
+
+        # html.body.append(self.makeStructures())
+
+        # html.body.append(self.makeCustom())
+
+        # return html.prettify(formatter="html5")
+        # return str(html)
+        return self.indent_html(html)
+
+    def formatStr(self, s):
+        """
+        Formats the string for use in schema
+        :param s: string to format
+        :type s: str
+        :return: formatted string
+        :rtype str
+        """
+        if s == '*':
+            return 'unknown'
+        else:
+            return re.sub(r'[\- ]', '_', s)
+
+    def makeHeader(self):
+        """
+        Create the header for the schema
+        :return: header for schema
+        :rtype object - BeautifulSoup
+        """
+        header_html = BeautifulSoup('', 'lxml')
+        # header = ['; meta: {} - {}'.format(k, re.sub(r'(^\"|\"$)', '', json.dumps(Utils.defaultDecode(v)))) for k, v in self._meta.items()]
+
+        header = header_html.new_tag('h2')
+        header.string = 'Schema'
+        header_html.append(header)
+
+        meta_table = header_html.new_tag('table')
+        for k, v in self._meta.items():
+            tr = header_html.new_tag('tr')
+
+            key = header_html.new_tag('td')
+            key.string = k
+            key['class'] = 'n'
+            tr.append(key)
+
+            value = header_html.new_tag('td')
+            if type(v) is list:
+                value.string = ', '.join(['**{}**: {}'.format(*imp) for imp in v] if type(v[0]) is list else v)
+            else:
+                value.string = v
+            value['class'] = 's'
+            tr.append(value)
+
+            meta_table.append(tr)
+
+        header_html.append(meta_table)
+
+        return header_html
+
+    def makeStructures(self):
+        """
+        Create the type definitions for the schema
+        :return: type definitions for the schema
+        :rtype str
+        """
+        tmp = ''
+        for t in self._types:
+            df = self._structFormats.get(t[1], None)
+
+            if df is not None:
+                tmp += df(t)
+        return tmp
+
+    def makeCustom(self):
+        defs = []
+        for field in self._custom:
+            line = '{name} = {type} ; {com}'.format(
+                name=self.formatStr(field[0]),
+                type=self._fieldType(field[1]),
+                com=field[-1]
+            )
+            defs.append(line)
+
+        return '\n'.join(defs)
+
+    def _fieldType(self, f):
+        """
+        Determines the field type for the schema
+        :param f: current type
+        :return: type mapped to the schema
+        :rtype str
+        """
+        if f in self._customFields:
+            rtn = self.formatStr(f)
+
+        elif f in self._fieldMap.keys():
+            rtn = self.formatStr(self._fieldMap.get(f, f))
+
+        else:
+            rtn = 'bstr'
+
+        # print(f, rtn)
+        return rtn
+
+    def _formatComment(self, msg, **kargs):
+        com = ';'
+        if msg not in ['', None, ' ']:
+            com += ' {msg}'.format(msg=msg)
+
+        for k, v in kargs.items():
+            com += ' #{k}:{v}'.format(
+                k=k,
+                v=json.dumps(v)
+            )
+
+        return com
+
+    # Structure Formats
+    def _formatRecord(self, itm):
+        """
+        Formats records for the given schema type
+        :param itm: record to format
+        :return: formatted record
+        :rtype str
+        """
+        lines = []
+        i = 1
+        for l in itm[-1]:
+            opts = {'type': l[2], 'field': l[0]}
+            if len(l[-2]) > 0: opts['options'] = fopts_s2d(l[-2])
+
+            lines.append('{idn}{pre_opts}{name}: {fType}{c} {com}\n'.format(
+                idn=self.indent,
+                pre_opts='? ' if '[0' in l[-2] else '',
+                name=self.formatStr(l[1]),
+                fType=self._fieldType(l[2]),
+                c=',' if i < len(itm[-1]) else '',
+                com=self._formatComment('' if l[-1] == '' else l[-1], jadn_opts=opts)
+            ))
+            i += 1
+        opts = {'type': itm[1]}
+        if len(itm[2]) > 0: opts['options'] = topts_s2d(itm[2])
+
+        return '\n{name} = {{ {com}\n{req}}}\n'.format(
+            name=self.formatStr(itm[0]),
+            com=self._formatComment('' if itm[-2] == '' else itm[-2], jadn_opts=opts),
+            req=''.join(lines)
+        )
+
+    def _formatChoice(self, itm):
+        """
+        Formats choice for the given schema type
+        :param itm: choice to format
+        :return: formatted choice
+        :rtype str
+        """
+        lines = []
+        i = 1
+        for l in itm[-1]:
+            opts = {'type': l[2], 'field': l[0]}
+            if len(l[-2]) > 0: opts['options'] = fopts_s2d(l[-2])
+
+            lines.append('{name}: {type}{c} {com}'.format(
+                name=self.formatStr(l[1]),
+                type=self._fieldType(l[2]),
+                c=' //' if i < len(itm[-1]) else '',
+                com=self._formatComment('' if l[-1] == '' else l[-1], jadn_opts=opts)
+            ))
+            i += 1
+
+        opts = {'type': itm[1]}
+        if len(itm[2]) > 0: opts['options'] = topts_s2d(itm[2])
+
+        return '\n{name} = ( {com}\n{idn}{defs}\n)\n'.format(
+            name=self.formatStr(itm[0]),
+            com=self._formatComment('' if itm[-2] == '' else itm[-2], jadn_opts=opts),
+            idn=self.indent,
+            defs='\n{}'.format(self.indent).join(lines)
+        )
+
+    def _formatMap(self, itm):
+        """
+        Formats map for the given schema type
+        :param itm: map to format
+        :return: formatted map
+        :rtype str
+        """
+        lines = []
+        i = 1
+        for l in itm[-1]:
+            opts = {'type': l[2], 'field': l[0]}
+            if len(l[-2]) > 0: opts['options'] = fopts_s2d(l[-2])
+
+            lines.append('{idn}{pre_opts}{name}: {fType}{c} {com}\n'.format(
+                idn=self.indent,
+                pre_opts='? ' if '[0' in l[-2] else '',
+                name=self.formatStr(l[1]),
+                fType=self._fieldType(l[2]),
+                c=',' if i < len(itm[-1]) else '',
+                com=self._formatComment('' if l[-1] == '' else l[-1], jadn_opts=opts)
+            ))
+            i += 1
+
+        opts = {'type': itm[1]}
+        if len(itm[2]) > 0: opts['options'] = topts_s2d(itm[2])
+
+        return '\n{name} = [ {com}\n{defs}]\n'.format(
+            name=self.formatStr(itm[0]),
+            com=self._formatComment('' if itm[-2] == '' else itm[-2], jadn_opts=opts),
+            defs=''.join(lines)
+        )
+
+    def _formatEnumerated(self, itm):
+        """
+        Formats enum for the given schema type
+        :param itm: enum to format
+        :return: formatted enum
+        :rtype str
+        """
+        lines = []
+        for l in itm[-1]:
+            opts = {'field': l[0]}
+
+            lines.append('\"{name}\" {com}\n'.format(
+                name=self.formatStr(l[1] or 'Unknown_{}_{}'.format(self.formatStr(itm[0]), l[0])),
+                com=self._formatComment('' if l[-1] == '' else l[-1], jadn_opts=opts)
+            ))
+
+        opts = {'type': itm[1]}
+        if len(itm[2]) > 0: opts['options'] = topts_s2d(itm[2])
+
+        return '\n{com}\n{init}{rem}'.format(
+            com=self._formatComment('' if itm[-2] == '' else itm[-2], jadn_opts=opts),
+            init='{} = '.format(self.formatStr(itm[0])),
+            rem='{} /= '.format(self.formatStr(itm[0])).join(lines)
+        )
+
+    def _formatArray(self, itm):  # TODO: what should this do??
+        """
+        Formats array for the given schema type
+        :param itm: array to format
+        :return: formatted array
+        :rtype str
+        """
+        field_opts = topts_s2d(itm[2])
+
+        field_type = '[{min}*{max} {type}]'.format(
+            min=field_opts.get('min', ''),
+            max=field_opts.get('max', ''),
+            type=self.formatStr(field_opts.get('rtype', 'string'))
+        )
+
+        return '\n{name} = {type} {com}\n'.format(
+            name=self.formatStr(itm[0]),
+            type=field_type,
+            com=self._formatComment(itm[-1])
+        )
+
+    def _formatArrayOf(self, itm):  # TODO: what should this do??
+        """
+        Formats arrayof for the given schema type
+        :param itm: arrayof to format
+        :return: formatted arrayof
+        :rtype str
+        """
+        field_opts = topts_s2d(itm[2])
+
+        field_type = '[{min}*{max} {type}]'.format(
+            min=field_opts.get('min', ''),
+            max=field_opts.get('max', ''),
+            type=self.formatStr(field_opts.get('rtype', 'string'))
+        )
+
+        return '\n{name} = {type} {com}\n'.format(
+            name=self.formatStr(itm[0]),
+            type=field_type,
+            com=self._formatComment(itm[-1])
+        )
+
+
+def html_dumps(jadn):
+    """
+    Produce CDDL schema from JADN schema
+    :arg jadn: JADN Schema to convert
+    :type jadn: str or dict
+    :return: Protobuf3 schema
+    :rtype str
+    """
+    return JADNtoHTML(jadn).html_dump()
+
+
+def html_dump(jadn, fname, source=""):
+    with open(fname, "w") as f:
+        if source:
+            f.write("<!-- Generated from " + source + ", " + datetime.ctime(datetime.now()) + "-->\n")
+        f.write(html_dumps(jadn))
