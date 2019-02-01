1cd239fad59487b20f95e94aefc174fdbbad8aee
Jerome Czachor <jerome.czachor@g2-inc.com>
2019-02-01 09:08:12 -0500

    Updated CI for python3 build

--- pkg/setup.py
+++ pkg/setup.py
@@ -2,34 +2,37 @@ import json
 
 from setuptools import setup, find_packages
 
-versionData = json.loads(open('version.json', 'r').read())
+with open('version.json', 'r') as version:
+    versionData = json.loads(version.read())
 
 setup(
     name=versionData['name'],
 
     version='{major}.{minor}.{bugfix}'.format(**versionData['version']['number']),
 
-    description='JADN Schema/Message Translator & Validator',
-    # long_description="The Server for NetVamp, that provides the REST API, controllers, and database.",
-
     # author='G2-Inc.',
     # author_email='screaming_bunny@g2-inc.com',
 
+    description='JADN Schema/Message Translator & Validator',
+    # long_description="",
+    # long_description_content_type="text/markdown",
+
+    # url="https://github.com/oasis-open/openc2-jadn",
+
+    install_requires=[d.replace('\n', '') for d in open('requirements.txt', 'r').readlines()],
+
+    packages=find_packages(),
+
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 5 - Development/Beta',
         "License :: Apache 2.0 License",
         'Intended Audience :: Information Technology',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
         'Topic :: Security :: Translation :: Validation'
     ],
 
-    packages=find_packages(),
-
-    install_requires=[d.replace('\n', '') for d in open('requirements.txt', 'r').readlines()],
-
-    # Python 2.7, 3.6+ but not 4
-    python_requires='>=2.7, !=3.[1-5], <4',
+    # Python 3.6+ but not 4
+    python_requires='>3.6, <4',
 
     package_data={
         str(versionData['name']): [
